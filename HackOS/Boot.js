1c520673afba94c2ac542b411c2e89d9d3b4d7ab
Oblivionburn <donney.lane@gmail.com>
2022-12-10 13:38:31 -0600

    v4.7

--- HackOS/Boot.js
+++ HackOS/Boot.js
@@ -1,5 +1,11 @@
+/*
+	Starts running all hardware
+	RAM Cost: 2.90GB
+*/
+
 /** @param {NS} ns */
 export async function main(ns)
 {
-    
+	ns.exec("/HackOS/Bus.js", "home");
+	ns.exec("/HackOS/RAM.js", "home");
 }
