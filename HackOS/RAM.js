6a76de168bac19ef48ca56066775b6e603865a54
Oblivionburn <donney.lane@gmail.com>
2022-12-10 13:31:45 -0600

    v4.6

--- HackOS/RAM.js
+++ HackOS/RAM.js
@@ -1,50 +1,90 @@
 /*
     Holds data in memory for other hardware to request
+	RAM Cost: 1.60GB
 */
 
-import {Bus} from "Bus.js";
-import {Packet} from "Packet.js";
-import {Data} from "Data.js";
+import {Bus} from "./HackOS/Bus.js";
+import {Packet} from "./HackOS/Packet.js";
+import {Data} from "./HackOS/Data.js";
 
 export var memory = [];
 
 /** @param {NS} ns */
 export async function main(ns)
 {
+    ns.disableLog("ALL");
+    ns.tail(ns.getScriptName(), "home");
+
     while (true)
     {
-        let packet = Receive(ns);
+        ns.clearLog();
+        ns.print("Packets in memory: " + memory.length);
+
+        let packet = CheckReceived(ns);
         if (packet != null)
         {
-            if (packet.request == "SAVE")
+            if (packet.Request == "SAVE")
             {
-                let dataObject = Object.assign(Data.prototype, JSON.parse(packet.data));
-                memory.push(dataObject);
+                let data = Object.assign(Data.prototype, JSON.parse(packet.Data));
+                memory.push(data);
+                ns.print("Received '" + data.Name + "' data from " + packet.Source + ".");
             }
-        }
+			else if (packet.Request == "LOAD")
+			{
+				let dataObject = Object.assign(Data.prototype, JSON.parse(packet.data));
+                ns.print("Received request for '" + dataObject.Name + "' data from " + packet.Source + ".");
 
-        await ns.sleep(1000);
+				for (let i = 0; i < memory.length; i++)
+				{
+					let memoryData = Object.assign(Data.prototype, memory[i]);
+					if (memoryData.Name == dataObject.Name)
+					{
+						let newPacket = new Packet("REQUESTED DATA", "RAM", packet.Source, memoryData);
+                        if (Send(ns, newPacket))
+                        {
+                            ns.print("Sent '" + data.Name + "' data to: " + newPacket.Destination);
+                        }
+                        break;
+					}
+				}
+			}
+
+            await ns.sleep(200);
+        }
+        else
+        {
+            await ns.sleep(1);
+        }
     }
 }
 
-async function Receive(ns)
+async function CheckReceived(ns)
 {
-    let inputPort = ns.getPortHandle(Bus.portMap["RAM IN"]);
-    if (!inputPort.empty())
+    let portNum = Bus.portMap["RAM IN"];
+    if (portNum != null)
     {
-        let packet = JSON.parse(port.read());
-        packet = Object.assign(Packet.prototype, packet);
-        return packet;
+        let inputPort = ns.getPortHandle(portNum);
+        if (!inputPort.empty())
+        {
+            let object = JSON.parse(port.read());
+            let packet = Object.assign(Packet.prototype, object);
+            return packet;
+        }
     }
-
+    
     return null;
 }
 
-async function Send(ns, packetName, destination, data)
+async function Send(ns, packet)
 {
-    let outputPort = ns.getPortHandle(Bus.portMap["RAM OUT"]);
-    let packet = new Packet(packetName, "RAM", destination, data);
-    let packetData = JSON.stringify(packet);
+    let portNum = Bus.portMap["RAM OUT"];
+    if (portNum != null)
+    {
+        let outputPort = ns.getPortHandle(portNum);
+        let packetData = JSON.stringify(packet);
+        
+        return outputPort.tryWrite(packetData);
+    }
     
-    return outputPort.tryWrite(packetData);
+    return false;
 }
