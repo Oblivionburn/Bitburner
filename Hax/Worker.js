9806b433c5e667f4b3008174707272a5335d01f8
Oblivionburn <donney.lane@gmail.com>
2022-12-15 00:04:18 -0600

    v7.7

--- Hax/Worker.js
+++ Hax/Worker.js
@@ -2,6 +2,16 @@
 export async function main(ns)
 {
     var target = ns.args[0];
+    ns.disableLog("ALL");
+
+    const colors = 
+    {
+        red: "\u001b[31;1m",
+        green: "\u001b[32;1m",
+        yellow: "\u001b[33;1m",
+        white: "\u001b[37;1m",
+        reset: "\u001b[0m"
+    };
 
     while (true)
     {
@@ -13,6 +23,17 @@ export async function main(ns)
 
         var securityLevel = ns.getServerSecurityLevel(target);
         var minSecurityLevel = ns.getServerMinSecurityLevel(target);
+
+        ns.clearLog();
+        ns.print(`${colors["yellow"] + target}`);
+		ns.print(`${colors["white"] + "Max Money: " + colors["green"] + "$" + maxMoney.toLocaleString()}`);
+		ns.print(`${colors["white"] + "Available Money: " + colors["green"] + "$" + availableMoney.toLocaleString()}`);
+		ns.print("\n");
+		ns.print(`${colors["white"] + "Min Security Level: " + colors["green"] + minSecurityLevel.toFixed(2)}`);
+		ns.print(`${colors["white"] + "Security Level: " + colors["green"] + securityLevel.toFixed(2)}`);
+		ns.print("\n");
+		ns.print(`${colors["white"] + "Required Hacking Level: " + colors["green"] + requiredHackLevel}`);
+		ns.print(`${colors["white"] + "Current Hacking Level: " + colors["green"] + hackLevel}`);
 
         if (securityLevel > minSecurityLevel)
         {
@@ -28,7 +49,7 @@ export async function main(ns)
         }
         else
         {
-            await ns.sleep(1000);
+            await ns.sleep(Math.random() * 1000);
         }
     }
 }
